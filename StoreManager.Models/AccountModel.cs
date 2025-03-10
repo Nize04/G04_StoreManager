cd32352b27c9e559949564d3447a0f4ce381da79
Nize <115048126+Nize04@users.noreply.github.com>
2025-03-10 20:04:21 +0400

    Add files via upload

---
+++ StoreManager.Models/AccountModel.cs
@@ -0,0 +1,8 @@
+﻿namespace StoreManager.Models
+{
+    public class AccountModel
+    {
+        public string Email { get; set; } = null!;
+        public IEnumerable<RoleModel> Roles { get; set; } = null!;
+    }
+}
