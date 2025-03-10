c599a2faac3b2e558c92782bf1f94f7fdcd5ea84
Nize <115048126+Nize04@users.noreply.github.com>
2025-03-10 20:57:00 +0400

    Update Account.cs

--- StoreManager.DTO/Account.cs
+++ StoreManager.DTO/Account.cs
@@ -1,4 +1,4 @@
-﻿using StoreManager.DTO.Interfaces;
+using StoreManager.DTO.Interfaces;
 using System.ComponentModel.DataAnnotations;
 namespace StoreManager.DTO;
 
@@ -10,6 +10,10 @@ public class Account : IDto
     [Required(ErrorMessage = "Email is required.")]
     [EmailAddress(ErrorMessage = "Invalid email format.")]
     public string Email { get; set; } = null!;
+
+    [Required(ErrorMessage = "username is required.")]
+    [StringLength(22, ErrorMessage = "username cannot be longer than 22 characters.")]
+    public string UserName { get; set; } = null!;
 
     [Required(ErrorMessage = "Password is required.")]
     [MinLength(6, ErrorMessage = "Password must be at least 6 characters long.")]
