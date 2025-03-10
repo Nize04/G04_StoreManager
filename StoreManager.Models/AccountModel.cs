de141cb1715b00e454f03c65584f08d36bdb0f37
Nize <115048126+Nize04@users.noreply.github.com>
2025-03-10 20:57:21 +0400

    Update AccountModel.cs

--- StoreManager.Models/AccountModel.cs
+++ StoreManager.Models/AccountModel.cs
@@ -1,8 +1,9 @@
-﻿namespace StoreManager.Models
+namespace StoreManager.Models
 {
     public class AccountModel
     {
         public string Email { get; set; } = null!;
+        public string UserName { get; set; } = null!;
         public IEnumerable<RoleModel> Roles { get; set; } = null!;
     }
 }
