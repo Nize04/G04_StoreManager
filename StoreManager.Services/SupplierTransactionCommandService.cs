60ea2ad66e36b79c7fbda41e399da04fd1e76f71
Nize <115048126+Nize04@users.noreply.github.com>
2025-03-13 04:09:07 +0400

    Update SupplierTransactionCommandService.cs

--- StoreManager.Services/SupplierTransactionCommandService.cs
+++ StoreManager.Services/SupplierTransactionCommandService.cs
@@ -1,7 +1,6 @@
 using Microsoft.Extensions.Logging;
 using StoreManager.DTO;
 using StoreManager.Facade.Interfaces.Repositories;
-using StoreManager.Facade.Interfaces.Services;
 
 namespace StoreManager.Services
 {
