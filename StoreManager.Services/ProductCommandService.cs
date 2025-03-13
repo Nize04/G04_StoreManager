24ef0f36507e92f0c03cba074f65e59d94b69b36
Nize <115048126+Nize04@users.noreply.github.com>
2025-03-13 04:06:42 +0400

    Update ProductCommandService.cs

--- StoreManager.Services/ProductCommandService.cs
+++ StoreManager.Services/ProductCommandService.cs
@@ -1,7 +1,6 @@
 using Microsoft.Extensions.Logging;
 using StoreManager.DTO;
 using StoreManager.Facade.Interfaces.Repositories;
-using StoreManager.Facade.Interfaces.Services;
 
 namespace StoreManager.Services
 {
@@ -20,8 +19,6 @@ namespace StoreManager.Services
         {
             _logger.LogInformation("Adding product with name: {ProductName}", product.Name);
 
-            await _unitOfWork.OpenConnectionAsync();
-
             try
             {
                 int productId = (int)await _unitOfWork.ProductRepository.InsertAsync(product);
@@ -33,17 +30,11 @@ namespace StoreManager.Services
                 _logger.LogError(ex, "An error occurred while adding product with name: {ProductName}", product.Name);
                 throw;
             }
-            finally
-            {
-                await _unitOfWork.CloseConnectionAsync();
-            }
         }
 
         public async Task UpdateProductAsync(Product product)
         {
             _logger.LogInformation("Updating product with ID: {ProductId}", product.Id);
-
-            await _unitOfWork.OpenConnectionAsync();
 
             try
             {
@@ -55,17 +46,11 @@ namespace StoreManager.Services
                 _logger.LogError(ex, "An error occurred while updating product with ID: {ProductId}", product.Id);
                 throw;
             }
-            finally
-            {
-                await _unitOfWork.CloseConnectionAsync();
-            }
         }
 
         public async Task DeleteProductAsync(int productId)
         {
             _logger.LogInformation("Deleting product with ID: {ProductId}", productId);
-
-            await _unitOfWork.OpenConnectionAsync();
 
             try
             {
@@ -76,10 +61,6 @@ namespace StoreManager.Services
             {
                 _logger.LogError(ex, "An error occurred while deleting product with ID: {ProductId}", productId);
                 throw;
-            }
-            finally
-            {
-                await _unitOfWork.CloseConnectionAsync();
             }
         }
     }
