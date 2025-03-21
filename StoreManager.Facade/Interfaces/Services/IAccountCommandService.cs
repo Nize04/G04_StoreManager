61c410bae4fefdeb4c0491c8dbb447bae14f83a6
Nize <115048126+Nize04@users.noreply.github.com>
2025-03-21 15:02:41 +0400

    Update IAccountCommandService.cs

--- StoreManager.Facade/Interfaces/Services/IAccountCommandService.cs
+++ StoreManager.Facade/Interfaces/Services/IAccountCommandService.cs
@@ -6,7 +6,7 @@ public interface IAccountCommandService
     Task<object> RegisterAsync(Account account);
     Task UpdateAccountAsync(Account account);
     Task<LoginResult> ProcessLoginAsync(string email, string password, string clientKey);
-    TwoFAResult Verify2FACode(string email, string code);
+    (string?, TwoFAResult) Verify2FACode(string code);
     Task AuthorizeAccountAsync(Account account);
     Task<bool> ChangePasswordAsync(int accountId, string oldPassword, string newPassword);
     Task EnableTwoFactorAuthAsync(int accountId, string password);
