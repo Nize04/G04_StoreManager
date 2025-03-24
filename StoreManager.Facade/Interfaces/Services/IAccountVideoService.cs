a0d84837bae009a7ff87502c3865180600ffd4de
Nize <115048126+Nize04@users.noreply.github.com>
2025-03-24 17:10:02 +0400

    Add files via upload

---
+++ StoreManager.Facade/Interfaces/Services/IAccountVideoService.cs
@@ -0,0 +1,10 @@
+﻿using StoreManager.DTO;
+
+namespace StoreManager.Facade.Interfaces.Services
+{
+    public interface IAccountVideoService
+    {
+        Task<int> UploadVideoAsync(AccountVideo accountVideo, Stream fileStream);
+        Task<byte[]> GetVideoByFileName(int accountId, string fileName);
+    }
+}
