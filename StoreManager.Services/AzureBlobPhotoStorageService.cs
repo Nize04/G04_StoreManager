57c3cd1c64c0f0b9fc7598aae61c106f98a8699c
Nize <115048126+Nize04@users.noreply.github.com>
2025-03-24 17:14:01 +0400

    Add files via upload

---
+++ StoreManager.Services/AzureBlobPhotoStorageService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using StoreManager.Facade.Interfaces.Services;
+using StoreManager.Models;
+
+namespace StoreManager.Services
+{
+    public class AzureBlobPhotoStorageService : AzureBlobStorageService,IAzureBlobPhotoStorageService
+    {
+        public AzureBlobPhotoStorageService(IOptions<AzureBlobPhotoStorageSettings> options, ILogger<AzureBlobPhotoStorageService> logger) : base(options, logger)
+        {
+        }
+    }
+}
---
+++ StoreManager.Services/AzureBlobVideoStorageService.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using StoreManager.Facade.Interfaces.Services;
+using StoreManager.Models;
+
+namespace StoreManager.Services
+{
+    public class AzureBlobVideoStorageService : AzureBlobStorageService,IAzureBlobVideoStorageService
+    {
+        public AzureBlobVideoStorageService(IOptions<AzureBlobVideoStorageSettings> options, ILogger<AzureBlobVideoStorageService> logger) : base(options, logger)
+        {
+            
+        }
+    }
+}
