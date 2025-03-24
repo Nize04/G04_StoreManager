2eb790e20fba6c7b863b99b798f566678de37769
Nize <115048126+Nize04@users.noreply.github.com>
2025-03-24 17:08:35 +0400

    Update IBlobStorageService.cs

--- StoreManager.Facade/Interfaces/Services/IBlobStorageService.cs
+++ StoreManager.Facade/Interfaces/Services/IBlobStorageService.cs
@@ -1,8 +1,8 @@
 ﻿namespace StoreManager.Facade.Interfaces.Services
 {
-    public interface IBlobStorageService:IAzureStorageService
-    {
-        string GenerateBlobUrl(string fileName);
-        Task<byte[]> DownloadFile(string fileName);
-    }
+     public interface IAzureBlobStorageService:IAzureStorageService
+     {
+         string GenerateBlobUrl(string fileName);
+         Task<byte[]> DownloadFile(string fileName);
+     }
 }
