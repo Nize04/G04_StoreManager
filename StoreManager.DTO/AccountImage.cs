5c2799611399563e0ee4daa16fbdd3a92685f88a
Nize <115048126+Nize04@users.noreply.github.com>
2025-03-24 17:04:58 +0400

    Update AccountImage.cs

--- StoreManager.DTO/AccountImage.cs
+++ StoreManager.DTO/AccountImage.cs
@@ -2,24 +2,7 @@
 
 namespace StoreManager.DTO
 {
-    public class AccountImage
+    public class AccountImage : AccountBlobFile
     {
-        [Key]
-        public int Id { get; set; }
-
-        [Required(ErrorMessage = "AccountId is required.")]
-        public int AccountId { get; set; }
-
-        [Required(ErrorMessage = "FileName is required.")]
-        [StringLength(255, ErrorMessage = "FileName cannot be longer than 255 characters.")]
-        public string FileName { get; set; } = null!;
-
-        [Required(ErrorMessage = "BlobUrl is required.")]
-        [Url(ErrorMessage = "Invalid URL format.")]
-        public string BlobUrl { get; set; } = null!;
-
-        public DateTime UploadedDate { get; set; }
-
-        public bool IsActive { get; set; }
     }
 }
