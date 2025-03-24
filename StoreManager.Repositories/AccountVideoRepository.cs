fab971fd66a4dc2e2e7d8ae5729a76a027b14846
Nize <115048126+Nize04@users.noreply.github.com>
2025-03-24 17:06:24 +0400

    Add files via upload

---
+++ StoreManager.Repositories/AccountVideoRepository.cs
@@ -0,0 +1,16 @@
+﻿using StoreManager.DTO;
+using StoreManager.Facade.Interfaces.Repositories;
+using System.Data;
+
+namespace StoreManager.Repositories
+{
+    internal class AccountVideoRepository : RepositoryBase<AccountVideo>, IAccountVideoRepository
+    {
+        public AccountVideoRepository(IDbConnection connection, IDbTransaction? transaction) : base(connection, transaction)
+        {
+        }
+
+        protected override string[] UnwantedPropertiesForInsert => new string[] { "Id", "IsActive", "UploadedDate" };
+        protected override string[] UnwantedPropertiesForUpdate => new string[] { "IsActive", "UploadedDate" };
+    }
+}
